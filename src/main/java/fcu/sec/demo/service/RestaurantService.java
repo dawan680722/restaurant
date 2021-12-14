faba91930776625e0172cfcd4c95dd4628894bb9
AiNi <D0813259@o365.fcu.edu.tw>
2021-12-15 03:29:44 +0800

    combine front-end and back-end, can run picture and restaurant on the first page.

---
+++ src/main/java/fcu/sec/demo/service/RestaurantService.java
@@ -0,0 +1,40 @@
+package fcu.sec.demo.service;
+
+import fcu.sec.demo.database.Sql2oDbHandler;
+import fcu.sec.demo.model.Restaurant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.sql2o.Connection;
+
+import java.util.List;
+
+@Service
+public class RestaurantService {
+
+  @Autowired
+  private Sql2oDbHandler sql2oDbHandler;
+
+  public RestaurantService() {
+
+  }
+
+  public List<Restaurant> getRestaurants() {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "select Area area, restaurantName restaurantName, picture picture, priceRange priceRange, restaurantIntroduce restaurantIntroduce"
+          + " from restaurantcsv";
+
+      return connection.createQuery(query).executeAndFetch(Restaurant.class);
+    }
+  }
+  public List<Restaurant> getRestaurants(String keyword){
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "select Area area, restaurantName restaurantName, picture picture, priceRange priceRange, restaurantIntroduce restaurantIntroduce"
+          + " from restaurantcsv where restaurantName like :keyword";
+
+      return connection.createQuery(query)
+          .addParameter("keyword", "%"+keyword+"%")
+          .executeAndFetch(Restaurant.class);
+    }
+  }
+
+}
